7ccdf0f57a4cf66d36e6d6deec07cba0114316a9
Darshan S <darshans2727@gmail.com>
2026-04-02 09:21:11 +0530

    refactor: add Main component and make header sticky with backdrop blur

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import { ModeToggle } from './mode-toggle';
 
 const Header = () => {
   return (
-    <header className="h-12 px-4 flex items-center justify-between border-b">
+    <header className="h-12 px-4 flex items-center justify-between border-b sticky z-10 top-0 bg-background/60 backdrop-blur-sm border-white/10 shadow-sm">
       <div className="header-left">
         <Link href={'/'}>Finance Tracker</Link>
       </div>
